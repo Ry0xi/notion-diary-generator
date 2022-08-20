4f319270ac9fc78460ccbf8c0b20c0ec9891332c
Ryoki Tanaka <ry_tanaka@yumemi.co.jp>
2022-08-21 00:57:06 +0900

    feat: 毎日1時に日記を作成する

---
+++ README.md
@@ -0,0 +1,6 @@
+# About
+## Notion Diary Generator
+バッチ処理で自動的に、Notionのデータベース上に日記レコードを作成します。
+
+## 参考記事
+https://zenn.dev/eetann/articles/2022-08-15-notion-api-with-github-actions
---
+++ src/index.ts
@@ -0,0 +1,62 @@
+import { Client } from "@notionhq/client";
+import dayjs from "dayjs";
+import ja from "dayjs/locale/ja";
+import timezone from "dayjs/plugin/timezone";
+import utc from "dayjs/plugin/utc";
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.locale(ja);
+
+const notion = new Client({
+  auth: process.env.NOTION_API_KEY
+});
+const DATABASE_ID: string = process.env.DATABASE_ID || "";
+const now = dayjs().tz("Asia/Tokyo");
+const YYYYMMDD = now.format("YYYY-MM-DD");
+const YYYYMMDDdd = now.format("YYYY月MM日DD(ddd)");
+
+(async () => {
+  const alreadyExist = await notion.databases.query({
+    database_id: DATABASE_ID,
+    filter: {
+      property: "Name",
+      title: {
+        equals: YYYYMMDDdd,
+      },
+    },
+  });
+  if (alreadyExist.results.length !== 0) {
+    console.log("すでに作成されています");
+    return;
+  }
+
+  try {
+    await notion.pages.create({
+      parent: {
+        database_id: DATABASE_ID,
+      },
+      properties: {
+        Title: {
+          title: [
+            {
+              text: {
+                content: YYYYMMDDdd,
+              },
+            },
+          ],
+        },
+        Date: {
+          type: "date",
+          date: {
+            start: YYYYMMDD,
+            end: null,
+          },
+        },
+      },
+    });
+    console.log("diary generated.");
+  } catch (e) {
+    console.log(e);
+  }
+})();
